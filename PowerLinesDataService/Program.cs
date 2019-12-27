4046a04fd5b3f96ceb063de7f1650d567c5c5776
John Watson <johnwatson484@hotmail.com>
2019-12-27 16:51:09 +0000

    Initial project added

---
+++ PowerLinesDataService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PowerLinesDataService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

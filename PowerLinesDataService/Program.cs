011536e711143bab1cf49daaf1df353f8540351a
John Watson <johnwatson484@hotmail.com>
2019-12-27 17:26:13 +0000

    Configuration setup added

--- PowerLinesDataService/Program.cs
+++ PowerLinesDataService/Program.cs
@@ -1,12 +1,50 @@
 ﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using PowerLinesDataService.Data;
 
 namespace PowerLinesDataService
 {
-    class Program
+    public static class Program
     {
-        static void Main(string[] args)
+        private static IServiceProvider serviceProvider;
+
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            RegisterServices();
+            DisposeServices();
+        }
+
+        private static void RegisterServices()
+        {
+            var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            IConfigurationRoot configuration = builder.Build();
+
+            var services = new ServiceCollection();
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+
+            serviceProvider = services.BuildServiceProvider();
+        }
+
+        private static void DisposeServices()
+        {
+            if (serviceProvider == null)
+            {
+                return;
+            }
+            if (serviceProvider is IDisposable)
+            {
+                ((IDisposable)serviceProvider).Dispose();
+            }
         }
     }
 }

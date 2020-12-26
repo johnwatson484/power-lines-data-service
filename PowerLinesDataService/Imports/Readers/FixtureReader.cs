a0b0ef2c280285038cd9ca4987f127f97b7ac3bd
John Watson <johnwatson484@hotmail.com>
2020-12-26 12:34:45 +0000

    Handle empty fixture line

--- PowerLinesDataService/Imports/Readers/FixtureReader.cs
+++ PowerLinesDataService/Imports/Readers/FixtureReader.cs
@@ -22,22 +22,24 @@ namespace PowerLinesDataService.Imports.Readers
                 int homeAverage = Array.IndexOf(headers, "AvgH");
                 int drawAverage = Array.IndexOf(headers, "AvgD");
                 int awayAverage = Array.IndexOf(headers, "AvgA");
-                                
+
                 while (!reader.EndOfStream)
                 {
                     var line = reader.ReadLine();
-                    var values = line.Split(',');
-
-                    fixtures.Add(new Fixture
+                    if (string.IsNullOrEmpty(line))
                     {
-                        Division = values[0].Trim(),
-                        Date = DateTime.Parse(values[1].Trim()).Add(TimeSpan.Parse(values[2])),
-                        HomeTeam = values[3].Trim(),
-                        AwayTeam = values[4].Trim(),
-                        HomeOddsAverage = homeAverage == -1 ? 0 : !string.IsNullOrEmpty(values[homeAverage].Trim()) ? decimal.Parse(values[homeAverage].Trim()) : 0,
-                        DrawOddsAverage = drawAverage == -1 ? 0 : !string.IsNullOrEmpty(values[drawAverage].Trim()) ? decimal.Parse(values[drawAverage].Trim()) : 0,
-                        AwayOddsAverage = awayAverage == -1 ? 0 : !string.IsNullOrEmpty(values[awayAverage].Trim()) ? decimal.Parse(values[awayAverage].Trim()) : 0
-                    });
+                        var values = line.Split(',');
+                        fixtures.Add(new Fixture
+                        {
+                            Division = values[0].Trim(),
+                            Date = DateTime.Parse(values[1].Trim()).Add(TimeSpan.Parse(values[2])),
+                            HomeTeam = values[3].Trim(),
+                            AwayTeam = values[4].Trim(),
+                            HomeOddsAverage = homeAverage == -1 ? 0 : !string.IsNullOrEmpty(values[homeAverage].Trim()) ? decimal.Parse(values[homeAverage].Trim()) : 0,
+                            DrawOddsAverage = drawAverage == -1 ? 0 : !string.IsNullOrEmpty(values[drawAverage].Trim()) ? decimal.Parse(values[drawAverage].Trim()) : 0,
+                            AwayOddsAverage = awayAverage == -1 ? 0 : !string.IsNullOrEmpty(values[awayAverage].Trim()) ? decimal.Parse(values[awayAverage].Trim()) : 0
+                        });
+                    }
                 }
             }
 
